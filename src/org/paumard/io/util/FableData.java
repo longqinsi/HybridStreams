bc8181a7383ab8a3a3c3db2fe328f5a97647ce0e
longqinsi <Charles.L2018@ucen.cc>
2019-03-11 11:35:18 +0800

    把Tab缩进转换为了空格

--- src/org/paumard/io/util/FableData.java
+++ src/org/paumard/io/util/FableData.java
@@ -4,36 +4,36 @@ import org.paumard.io.model.Fable;
 
 public class FableData {
 
-	private Fable fable;
-	private int offset, length;
-	
-	public FableData(Fable fable, int offset, int length) {
-		this.fable = fable;
-		this.offset = offset;
-		this.length = length;
-	}
-	
-	public Fable getFable() {
-		return fable;
-	}
+    private Fable fable;
+    private int offset, length;
 
-	public void setFable(Fable fable) {
-		this.fable = fable;
-	}
-	
-	public int getOffset() {
-		return offset;
-	}
-	
-	public void setOffset(int offset) {
-		this.offset = offset;
-	}
-	
-	public int getLength() {
-		return length;
-	}
-	
-	public void setLength(int length) {
-		this.length = length;
-	}
+    public FableData(Fable fable, int offset, int length) {
+        this.fable = fable;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    public Fable getFable() {
+        return fable;
+    }
+
+    public void setFable(Fable fable) {
+        this.fable = fable;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
